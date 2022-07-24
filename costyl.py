a60975fe00d10a90f158cc10fbd3896c9dc212f5
Poe <99239747+Morgan136@users.noreply.github.com>
2022-07-24 23:22:54 +0500

    unfinished, but closely

--- costyl.py
+++ costyl.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 
+q = ""
+with open("output.txt", "r") as r:
+	q += r.readline()
+	print("\nroot file: " + q)
 words = [w for w in Path("output.txt").read_text().replace("\n", " ").split("C:\\")]
 
+f = open("output.txt")	
+w = words [1]
+print("str for deleting: " + w)
+
 for i in words:
-	print(i)
+
+	i.replace(w, "", 1)
+
+for i in words:
+	print (i)
+#for z in words:
+#	print(z)
